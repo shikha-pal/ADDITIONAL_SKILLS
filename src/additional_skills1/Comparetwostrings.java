9782edfed67f5d2b0b2b0292560743358450922b
shikha-pal <shikhapal804@gmail.com>
2020-09-02 15:38:12 +0530

    This is my second  commit to this repository

---
+++ src/additional_skills1/Comparetwostrings.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package additional_skills1;
+
+/**
+ *
+ * @author 007
+ */
+public class Comparetwostrings {
+    
+}
---
+++ src/additional_skills1/Countvowels.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package additional_skills1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 007
+ */
+public class Countvowels {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       int count=0;
+        System.out.println("Enter a string:");
+       String str = sc.nextLine();
+       for(int i=0;i<str.length();i++)
+       {
+           char ch= str.charAt(i);
+           if(ch=='A'||ch=='a'||ch=='E'||ch=='e'||ch=='I'||ch=='i'||ch=='O'||ch=='o'||ch=='U'||ch=='u')
+                count++;
+       }
+        System.out.println("No. of vowels:"+count);
+           
+               
+               
+               
+               
+       }
+    }     
+
