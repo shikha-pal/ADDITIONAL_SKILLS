4b584f70d5a3602315fbc7edddf10a03e2ea8446
shikha-pal <58381197+shikha-pal@users.noreply.github.com>
2020-10-02 14:53:34 +0530

    Add files via upload
    fifth commit

---
+++ POINTER1.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+
+static int a[]={0,1,2,3,4};
+static  int *p[]={a,a+1,a+2,a+3,a+4};
+ int **ptr=p;
+ clrscr();
+ printf("%d, %d\n",a,*a); // 170,0
+ printf(" %d, %d, %d\n",p,*p,**p); //180,170,0
+ printf("%d, %d, %d\n",ptr,*ptr,**ptr);   //180,170,0
+ getch();
+}
